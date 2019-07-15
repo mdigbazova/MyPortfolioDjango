e0a4d9f77d10cba6a4f5b4c88fefeaeba9c0fb4c
Mariya Digbazova <mdigbazova@gmail.com>
2019-07-15 18:27:56 +0300

    chng

--- MeryPortfolio-project/MeryPortfolio/wsgi.py
+++ MeryPortfolio-project/MeryPortfolio/wsgi.py
@@ -15,4 +15,4 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MeryPortfolio.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise('jobs')
+application = DjangoWhiteNoise(application)

bd80c38df53bf2b55ae666dfa2747a1500d2c70d
Mariya Digbazova <mdigbazova@gmail.com>
2019-07-16 17:27:36 +0300

    chng

--- MeryPortfolio-project/MeryPortfolio/wsgi.py
+++ MeryPortfolio-project/MeryPortfolio/wsgi.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-#from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MeryPortfolio.settings')
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MeryPortfolio.settings')
 
 application = get_wsgi_application()
-#application = DjangoWhiteNoise(application)
+

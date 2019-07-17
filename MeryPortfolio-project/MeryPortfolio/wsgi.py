41d71522402d8e0586a01ed66d5ebc2a3d45959d
Mariya Digbazova <mdigbazova@gmail.com>
2019-07-17 14:53:59 +0300

    cng

--- MeryPortfolio-project/MeryPortfolio/wsgi.py
+++ MeryPortfolio-project/MeryPortfolio/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os, sys
 from django.core.wsgi import get_wsgi_application
-from . import settings
+#from . import settings
 
 # path = '/MyPortfolioDjango/MeryPortfolio-project/MeryPortfolio'
 # if path not in sys.path:
@@ -25,7 +25,7 @@ from . import settings
 
 #os.environ['DJANGO_SETTINGS_MODULE'] = 'MeryPortfolio.settings'
 #os.environ['DJANGO_SETTINGS_MODULE'] = 'MeryPortfolio.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MeryPortfolio.settings')
 
 application = get_wsgi_application()
 

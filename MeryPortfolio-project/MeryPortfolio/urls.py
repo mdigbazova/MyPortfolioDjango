05d82f5e6453976d65a19fa7a954c60a4da0f4c1
Mariya Digbazova <mdigbazova@gmail.com>
2019-07-24 17:20:39 +0300

    cng

--- MeryPortfolio-project/MeryPortfolio/urls.py
+++ MeryPortfolio-project/MeryPortfolio/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-import jobs.views
+from .. import jobs
 
 urlpatterns = [
     path('admin/', admin.site.urls),

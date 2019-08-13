a1b0e4d51362e682193a7e7d900ab708c42f990c
Mariya Digbazova <mdigbazova@gmail.com>
2019-08-13 15:07:22 +0300

    Last version which works on DigitalOcean

--- portfolioproject/jobs/models.py
+++ portfolioproject/jobs/models.py
@@ -5,10 +5,12 @@ from django.db import models
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=200, default='')
-    summary = models.TextField(max_length=2000)
+    project_description = models.TextField(max_length=1000, default='')
+    responsibilities = models.TextField(max_length=1000, default='')
+    technologies = models.TextField(max_length=1000, default='')
 
     class Meta:
-        ordering = ('title', 'summary')
+        ordering = ('-title', 'project_description')
 
     def __str__(self):
         return self.title
